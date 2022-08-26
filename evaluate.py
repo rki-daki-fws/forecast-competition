33a5cbcc6a3324e08da801a834ab616fac506741
Moritz Bach <BachM@rki.de>
2022-08-26 10:52:36 +0200

    dummy evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,2 +1,10 @@
-print("Evaluation passed?")
-print(True == True)
+import pandas as pd
+import numpy as np
+
+gt = pd.read_csv("challenge-data/groundtruth.csv").to_numpy()
+pred = pd.read_csv("challenge-results/dummy-test.csv").to_numpy()
+
+mse = np.square(gt[:, 1] - pred[:, 1]).mean()
+
+print("Evaluation results:")
+print(mse)
